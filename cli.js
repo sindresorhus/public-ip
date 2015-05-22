d09f0e1b172ae49291af4fbc2299f7cf94084502
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-22 21:33:52 +0200

    Small tweaks

--- cli.js
+++ cli.js
@@ -13,7 +13,8 @@ meow({
 
 publicIp(function (err, ip) {
 	if (err) {
-		throw err;
+		console.error(err.message);
+		process.exit(1);
 	}
 
 	console.log(ip);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 var dns = require('native-dns');
 
 module.exports = function (cb) {
