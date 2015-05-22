7ee730f5873de052ba715d8a68543f4a73ad42f5
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-22 18:24:04 +0200

    Use `meow` in the CLI

--- cli.js
+++ cli.js
@@ -1,29 +1,15 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var publicIp = require('./');
-var argv = process.argv.slice(2);
 
-function help() {
-	console.log([
-		'',
-		'  ' + pkg.description,
-		'',
-		'  Example',
-		'    $ public-ip',
-		'    46.5.21.123'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+meow({
+	help: [
+		'Example',
+		'  $ public-ip',
+		'  46.5.21.123'
+	].join('\n')
+});
 
 publicIp(function (err, ip) {
 	if (err) {
