a69c91ce57d0fa7350c3f02c83674dd0a73388c7
Subin Siby <mail@subinsb.com>
2020-06-19 02:22:55 +0530

    Fix Lint warnings

--- test-browser.js
+++ test-browser.js
@@ -12,17 +12,17 @@ test('v6', async t => {
 
 		t.equal(ip1, v6Address);
 		t.end();
-	}).catch(err => {
-		if (err === 'Couldn\'t find your IP') {
+	}).catch(error => {
+		if (error === 'Couldn\'t find your IP') {
 			v6Address = false;
 		}
+
 		t.end();
 	});
 });
 
 test('v4', async t => {
 	publicIp.v4().then(async ip1 => {
-		console.log(ip1)
 		const ip2 = await publicIp.v4({
 			fallbackUrls: [
 				'https://ifconfig.me'
@@ -37,12 +37,12 @@ test('v6or4', async t => {
 	publicIp.v6or4().then(async ip1 => {
 		const ip2 = await publicIp.v4();
 
-		if (v6Address !== false) {
-			t.equal(ip1, v6Address);
-		} else {
+		if (v6Address === false) {
 			t.equal(ip1, ip2);
+		} else {
+			t.equal(ip1, v6Address);
 		}
-	
+
 		t.end();
 	});
 });
