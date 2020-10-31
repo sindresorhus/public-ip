c1f33362cbdecbd622405008147fa5b06f3ec74f
Daniel Kostro <kostro.d@gmail.com>
2020-10-31 18:06:03 +0100

    chore: fix linting errors

--- browser.js
+++ browser.js
@@ -3,9 +3,9 @@ const isIp = require('is-ip');
 
 const defaults = {
 	timeout: 5000
-  };
+};
 
-  const urls = {
+const urls = {
 	v4: [
 		'https://ipv4.icanhazip.com/',
 		'https://api.ipify.org/'
@@ -16,55 +16,58 @@ const defaults = {
 	]
 };
 
-  const sendXhr = (url, options, version) => {
+const sendXhr = (url, options, version) => {
 	const xhr = new XMLHttpRequest();
 	let _resolve;
 	const promise = new Promise((resolve, reject) => {
-	  _resolve = resolve;
-	  xhr.addEventListener('error', reject, { once: true });
-	  xhr.addEventListener('timeout', reject, { once: true });
+		_resolve = resolve;
+		xhr.addEventListener('error', reject, {once: true});
+		xhr.addEventListener('timeout', reject, {once: true});
 
-	  xhr.addEventListener('load', () => {
-		  const ip = xhr.responseText.trim();
+		xhr.addEventListener('load', () => {
+			const ip = xhr.responseText.trim();
 
-		  if (!ip || !isIp[version](ip)) {
-			reject();
-			return;
-		  }
+			if (!ip || !isIp[version](ip)) {
+				reject();
+				return;
+			}
 
-		  resolve(ip);
+			resolve(ip);
 		}, {once: true});
 
-	  xhr.open('GET', url);
-	  xhr.timeout = options.timeout;
-	  xhr.send();
+		xhr.open('GET', url);
+		xhr.timeout = options.timeout;
+		xhr.send();
 	});
 	promise.cancel = () => {
-	  _resolve();
-	  xhr.abort();
-	}
-	return promise;
-  };
+		_resolve();
+		xhr.abort();
+	};
 
-  const queryHttps = (version, options) => {
+	return promise;
+};
+
+const queryHttps = (version, options) => {
 	let request;
 	const promise = (async function () {
-	  const urls_ = [].concat.apply(urls[version], options.fallbackUrls || []);
-	  for (const url of urls_) {
-		try {
-		  // eslint-disable-next-line no-await-in-loop
-		  request = sendXhr(url, options, version);
-		  const ip = await request;
-		  return ip;
-		} catch (_) {}
-	  }
-	  throw new Error("Couldn't find your IP");
+		const urls_ = [].concat.apply(urls[version], options.fallbackUrls || []);
+		for (const url of urls_) {
+			try {
+				request = sendXhr(url, options, version);
+				// eslint-disable-next-line no-await-in-loop
+				const ip = await request;
+				return ip;
+			} catch (_) {}
+		}
+
+		throw new Error('Couldn\'t find your IP');
 	})();
 	promise.cancel = () => {
-	  request.cancel();
+		request.cancel();
 	};
+
 	return promise;
-  };
+};
 
 module.exports.v4 = options => queryHttps('v4', {...defaults, ...options});
 
